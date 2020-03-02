17d846eed728b722ea3353c48e255fc2b0f4cff1
Javs <javs.doblerodriguez@gmail.com>
2020-03-02 13:25:08 +0100

    Inicio del código

---
+++ Práctica 1/P01b.py
@@ -0,0 +1,11 @@
+# Práctica 01.b
+# Metaheurísticas (MH) - Grupo A1
+# Universidad de Granada - Grado en Ingeniería Informática - Curso 2019/2020
+# Javier Rodríguez Rodríguez - @doblerodriguez
+
+from numpy.random import shuffle
+
+def Greedy_COPKM(X, R, k, mu):
+    RSI = shuffle()
+
+
