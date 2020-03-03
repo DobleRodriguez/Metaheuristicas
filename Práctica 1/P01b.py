b28891220eb3a6f2dee69e36075615ac9f22fde2
Javs <javs.doblerodriguez@gmail.com>
2020-03-03 14:18:27 +0100

    Lectura correcta de path de datos independiente de cwd

--- Práctica 1/P01b.py
+++ Práctica 1/P01b.py
@@ -1,18 +1,28 @@
 # Práctica 01.b
+# Técnicas de Búsqueda Local y Algoritmos Greedy para el Problema del Agrupamiento con Restricciones
 # Metaheurísticas (MH) - Grupo A1
 # Universidad de Granada - Grado en Ingeniería Informática - Curso 2019/2020
 # Javier Rodríguez Rodríguez - @doblerodriguez
 
-from numpy.random import shuffle
+import numpy as np
 import pandas as pd
+import pathlib as pl
 
-iris_data = pd.read_csv("Práctica 1/Instancias y Tablas PAR 2019-20/iris_set.dat", header=None)
-iris_const_10 = pd.read_csv() #Fichero
-iris_const_20 = pd.read_csv() #FIchero
+def read_data(set_name):
+    data = pd.read_csv(pl.Path(__file__).parent / f"Instancias y Tablas PAR 2019-20/{set_name}_set.dat", header=None)
+    const_10 = pd.read_csv(pl.Path(__file__).parent / f"Instancias y Tablas PAR 2019-20/{set_name}_set_const_10.const", header=None) 
+    const_20 = pd.read_csv(pl.Path(__file__).parent / f"Instancias y Tablas PAR 2019-20/{set_name}_set_const_20.const", header=None) 
+    return data, const_10, const_20
 
+def greedy_copkm(data, const, k, centroid):
+    RSI = np.random.RandomState.shuffle(np.arange(len(data)))
+    for i in RSI:
+        pass
 
-def greedy_copkm(X, R, k, mu):
-    RSI = shuffle()
+def local_search():
+    pass
 
+# Iris
+data, const_10, const_20 = read_data("iris")
 
-
+    
