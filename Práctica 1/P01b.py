c4689a45f93582783548d4afea2d3d51a25526a4
Javs <javs.doblerodriguez@gmail.com>
2020-03-09 17:39:34 +0100

    Completado mecanismo de conteo de restricciones en Greedy

--- Práctica 1/P01b.py
+++ Práctica 1/P01b.py
@@ -9,18 +9,21 @@ import pandas as pd
 import pathlib as pl
 
 def read_data(set_name):
-    data = pd.read_csv(pl.Path(__file__).parent / f"Instancias y Tablas PAR 2019-20/{set_name}_set.dat", header=None)
-    const_10 = pd.read_csv(pl.Path(__file__).parent / f"Instancias y Tablas PAR 2019-20/{set_name}_set_const_10.const", header=None) 
-    const_20 = pd.read_csv(pl.Path(__file__).parent / f"Instancias y Tablas PAR 2019-20/{set_name}_set_const_20.const", header=None) 
+    data = pd.read_csv(pl.Path(__file__).parent /
+    f"Instancias y Tablas PAR 2019-20/{set_name}_set.dat", header=None)
+    const_10 = pd.read_csv(pl.Path(__file__).parent /
+    f"Instancias y Tablas PAR 2019-20/{set_name}_set_const_10.const", header=None) 
+    const_20 = pd.read_csv(pl.Path(__file__).parent /
+    f"Instancias y Tablas PAR 2019-20/{set_name}_set_const_20.const", header=None) 
     return data, const_10, const_20
+
+def local_search():
+    pass
 
 def greedy_copkm(data, const, k, centroid):
     RSI = np.random.RandomState.shuffle(np.arange(len(data)))
     for i in RSI:
         pass
-
-def local_search():
-    pass
 
 # Iris
 data, const_10, const_20 = read_data("iris")
